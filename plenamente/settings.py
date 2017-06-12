8cfd32dd018e41ad8b797dae6a2455588fd2377b
adriansolanorobles <adrian.solano@protonmail.com>
2017-06-12 16:06:27 -0500

    agrege static root

--- plenamente/settings.py
+++ plenamente/settings.py
@@ -126,7 +126,7 @@ CART_SESSION_ID = 'cart'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(os.getcwd(),'static') ]
 
 
