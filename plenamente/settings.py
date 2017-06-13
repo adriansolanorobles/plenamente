12011cf1ab1fe355545480c029708600ce4bb71a
Ubuntu <ubuntu@ip-172-31-22-30.us-west-2.compute.internal>
2017-06-13 00:52:54 +0000

    deploy completo

--- plenamente/settings.py
+++ plenamente/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'judy4b3^wbfmq=0wiep)-r8%)nv#6#(5o+f72gg_%p=(x%lw^2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
